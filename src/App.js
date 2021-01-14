0fa49969ca5e7b3f1f028c24ae81decd5659a006
narrator-fiction <narrator0120@gmail.com>
2021-01-15 07:01:51 +0800

    fix typo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./styles.css";
 
 const renderTime = ({ remainingTime }) => {
   if (remainingTime === 0) {
-    return <div className="timer">Too lale...</div>;
+    return <div className="timer">Too late...</div>;
   }
 
   return (
